06e474ae62aecb06d4ffc5cc2e1462030c896090
vladchimescu <vladhkim@gmail.com>
2015-09-30 11:03:51 +0200

    added a dependency to DESCRIPTION; processVignette() puts figure parameter settings as separate code blocks now

--- R/processTex.R
+++ R/processTex.R
@@ -10,12 +10,12 @@ processVignette <- function(filename, removeFigs = TRUE) {
   if(file_ext(filename) == "Rmd") {
     ## extract all code chunk headers with figure dimension settings
     ## and put them in comments
-    if (length(grep("# IRkernel", readLines(filename), fixed = TRUE)) == 0) {
+    if (length(grep("IRkernel", readLines(filename), fixed = TRUE)) == 0) {
       text <- gsub("(^[\t >]*```+\\s*\\{[.]?)([a-zA-Z]+.*)(fig+.*)\\}\\s*$", 
-                   "\\1\\2\\3\\}\n# IRkernel::set_plot_options\\(\\2\\3\\)", 
+                   "IRkernel::set_plot_options\\(\\2\\3\\)\n\\1\\2\\3\\}", 
                    readLines(filename))
-      text <- gsub("(# IRkernel::set_plot_options\\()([a-zA-Z]+.*)(width+.*|height+.*)fig.(height+.*|width+.*)\\)", 
-                   "\\1\\3\\4\\)", text)
+      text <- gsub("(IRkernel::set_plot_options\\()([a-zA-Z]+.*)(width+.*|height+.*)fig.(height+.*|width+.*)\\)", 
+                   "```{r}\n\\1\\3\\4\\)\n```", text)
       writeLines(text, con = filename, sep = "\n")
     }
 
@@ -24,11 +24,11 @@ processVignette <- function(filename, removeFigs = TRUE) {
   else if(file_ext(filename) == "Rnw") {
     ## extract all code chunk headers with figure dimension settings
     ## and put them in comments
-    if (length(grep("# IRkernel", readLines(filename), fixed = TRUE)) == 0) {
+    if (length(grep("IRkernel", readLines(filename), fixed = TRUE)) == 0) {
       text <- gsub("(^\\s*<<)(.*)(fig+.*)>>=.*$", 
-                   "\\1\\2\\3>>=\n# IRkernel::set_plot_options\\(\\2\\3\\)", readLines(filename))
-      text <- gsub("(# IRkernel::set_plot_options\\()([a-zA-Z]+.*)(width+.*|height+.*)fig.(height+.*|width+.*)\\)", 
-                   "\\1\\3\\4\\)", text)
+                   "IRkernel::set_plot_options\\(\\2\\3\\)\n\\1\\2\\3>>=", readLines(filename))
+      text <- gsub("(IRkernel::set_plot_options\\()([a-zA-Z]+.*)(width+.*|height+.*)fig.(height+.*|width+.*)\\)", 
+                   "<<\\3\\4>>=\n\\1\\3\\4\\)\n@", text)
       writeLines(text, con = filename, sep = "\n")
     }
   }
