1ffaa190ec22e77a85005ad938c24c9b7b1d3a26
vladchimescu <vladhkim@gmail.com>
2015-09-30 09:39:48 +0200

    made some changes in processTex for processing of figure parameters

--- R/processTex.R
+++ R/processTex.R
@@ -7,29 +7,33 @@ processTex <- function(filename, removeFigs = TRUE) {
 }
 
 processVignette <- function(filename, removeFigs = TRUE) {
-  ## so first need to figure out if the vignette is Rnw or Rmd
-  ## depending on this branch into two action modes for these formats
-  ## use file_ext() function from 'tools' package (which has to be loaded)
   if(file_ext(filename) == "Rmd") {
     ## extract all code chunk headers with figure dimension settings
-    # grep("^[\t >]*```+\\s*\\{[.]?([a-zA-Z]+.*)(fig+.*)\\}\\s*$", 
-         # readLines(filename), value=TRUE)
-    text <- gsub("(^[\t >]*```+\\s*\\{[.]?)([a-zA-Z]+.*)(fig+.*)\\}\\s*$", 
-                 "\\1\\2\\3\\}\n# \\2\\3", readLines(filename))
-    writeLines(text, con = filename, sep = "\n")
-    ## now need to add comments in code chunks with figure parameters
+    ## and put them in comments
+    if (length(grep("# IRkernel", readLines(filename), fixed = TRUE)) == 0) {
+      text <- gsub("(^[\t >]*```+\\s*\\{[.]?)([a-zA-Z]+.*)(fig+.*)\\}\\s*$", 
+                   "\\1\\2\\3\\}\n# IRkernel::set_plot_options\\(\\2\\3\\)", 
+                   readLines(filename))
+      text <- gsub("(# IRkernel::set_plot_options\\()([a-zA-Z]+.*)(width+.*|height+.*)fig.(height+.*|width+.*)\\)", 
+                   "\\1\\3\\4\\)", text)
+      writeLines(text, con = filename, sep = "\n")
+    }
+
   }
   
   else if(file_ext(filename) == "Rnw") {
     ## extract all code chunk headers with figure dimension settings
-    # grep("^\\s*<<(.*)(fig+.*)>>=.*$", readLines(filename), value = TRUE)
-    text <- gsub("(^\\s*<<)(.*)(fig+.*)>>=.*$", 
-                 "\\1\\2\\3>>=\n# \\2\\3", readLines(filename))
-    writeLines(text, con = filename, sep = "\n")
+    ## and put them in comments
+    if (length(grep("# IRkernel", readLines(filename), fixed = TRUE)) == 0) {
+      text <- gsub("(^\\s*<<)(.*)(fig+.*)>>=.*$", 
+                   "\\1\\2\\3>>=\n# IRkernel::set_plot_options\\(\\2\\3\\)", readLines(filename))
+      text <- gsub("(# IRkernel::set_plot_options\\()([a-zA-Z]+.*)(width+.*|height+.*)fig.(height+.*|width+.*)\\)", 
+                   "\\1\\3\\4\\)", text)
+      writeLines(text, con = filename, sep = "\n")
+    }
   }
   
   else {
-    ## do nothing and print an error, saying that only
-    ## Rmd and Rnw vignettes can be processed
-
+    cat("Only Rmd and Rnw formats are supported!")
   }
+}
